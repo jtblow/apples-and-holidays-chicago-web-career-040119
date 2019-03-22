490c6c0605215e1f9b1ff2b01cae0f52aed38127
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-22 17:37:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holidays|
   puts "#{season.capitalize}:"
-  holidays.each do |holiday, supp
+  holidays.each do |holiday, supplies|
+    
 end
 
 def all_holidays_with_bbq(holiday_hash)
