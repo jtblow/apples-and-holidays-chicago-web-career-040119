d4d127631f60fbb3eac82df198ca28d196a3fe9c
creative-zombie-4458 <jtbartlow@gmail.com>
2019-03-22 17:33:23 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+holiday_hash.each do |seasons, holidays|
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
